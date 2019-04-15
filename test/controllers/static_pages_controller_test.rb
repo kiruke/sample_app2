486ef4d8a2309364d185fb96be18dabf89dec7d8
kiruke <kiruke2525@gmail.com>
2019-04-15 03:13:07 +0000

    Complete advanced setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
@@ -16,7 +21,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
